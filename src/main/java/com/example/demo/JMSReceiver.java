42b163c6eb8c7af8c60aaf94b5531bc0bdcc0bea
Infyshine <Infyshine@DESKTOP-6>
2021-07-15 19:03:01 +0530

    receiver

---
+++ src/main/java/com/example/demo/JMSReceiver.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JMSReceiver implements ChannelAwareMessageListener {
+      
+	@Override
+	public void onMessage(Message message, com.rabbitmq.client.Channel channel) throws Exception {
+		System.out.println("Received <" + message + ">");
+		
+		byte[] byteArray = message.getBody();
+		Product product = (Product) getObject(byteArray);
+		System.out.println("product = " + product);
+		channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+	}
+	
+	private static Object getObject(byte[] byteArr) throws IOException, ClassNotFoundException{
+		ByteArrayInputStream bis = new ByteArrayInputStream(byteArr);
+		ObjectInput in = new ObjectInputStream(bis);
+		return in.readObject();
+	}
+}
